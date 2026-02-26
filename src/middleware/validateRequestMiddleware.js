e7632c678ec9c6b0369528225d99957deac4613c
argaaji120 <argaaji120@gmail.com>
2026-02-26 18:53:26 +0700

    feat - update validate request middleware

--- src/middleware/validateRequestMiddleware.js
+++ src/middleware/validateRequestMiddleware.js
@@ -2,17 +2,24 @@ export const validateRequest = (schema) => {
     return (req, res, next) => {
         const result = schema.safeParse(req.body);
 
+        // If validation fails
         if (!result.success) {
-            const formatted = result.error.format();
-            const flatErrors = Object.values(formatted)
-                .flat()
-                .filter(Boolean)
-                .map((err) => err._errors)
-                .flat();
+            // const formatted = result.error.format();
+            // const flatErrors = Object.values(formatted)
+            //     .flat()
+            //     .filter(Boolean)
+            //     .map((err) => err._errors)
+            //     .flat();
 
-            console.log(flatErrors);
+            // return res.status(400).json({ message: flatErrors.join(", ") });
 
-            return res.status(400).json({ message: flatErrors.join(", ") });
+            // Other approach
+            // Get all error messages in a simple way
+            const errors = result.error.issues.map((issue) => issue.message);
+
+            return res.status(400).json({
+                message: errors.join(", "),
+            });
         }
 
         next();
